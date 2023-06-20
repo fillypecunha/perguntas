5526c11d30aa7398eaa75cc4559c084d1b98f6c5
Fillype da Silva Macedo Cunha <116668901+fillypecunha@users.noreply.github.com>
2023-06-19 21:11:09 -0300

    Add files via upload

---
+++ Quiz2.0/perguntas.js
@@ -0,0 +1,49 @@
+
+
+export default[
+    
+        {
+             questions:"1) quem foi campeao na champions 2022: ",
+        answers:[
+           {option:"a) Liverpool ",gabarito:false },
+           {option:"b) Real Madrid ",gabarito:true},
+           {option:"c) Barcelona ",gabarito:false},
+           {option:"d) PSG ",gabarito:false}
+        ],
+    },
+    { questions:"2) quem foi campeao na Libertadores 2022: ",
+        answers:[
+           {option:"a) Boca Juniors ",gabarito:false },
+           {option:"b) River Plate ",gabarito:false},
+           {option:"c) Palmeiras ",gabarito:false},
+           {option:"d) Flamengo ",gabarito:true}
+        ],
+    },
+    { questions:"3) QUEM FOI CAMPEAO DA COPA DO MUNDO 2022 : ",
+        answers:[
+           {option:"a) França ",gabarito:false },
+           {option:"b) Argentina ",gabarito:true},
+           {option:"c) Brasil ",gabarito:false},
+           {option:"d) Alemanha ",gabarito:false}
+        ],
+    },
+    { questions:"4) QUAL TRIO TEM MAIS GOLS ATUANDO JUNTOS  : ",
+        answers:[
+           {option:"a) MESSI,NEYMAR,SUARES ",gabarito:false },
+           {option:"b) CR7,BALE,BENZEMA ",gabarito:false},
+           {option:"c) MANE,SALAH,FIRMINO ",gabarito:true},
+           {option:"d) Marco van Basten, Ruud Gullit, e Frank Rijkaard",gabarito:false}
+        ],
+    },{ questions:"5) quem foi bola de ouro do ano 2015 : ",
+    answers:[
+       {option:"a) Messi ",gabarito:false },
+       {option:"b) Salah ",gabarito:false},
+       {option:"c) Haland ",gabarito:false},
+       {option:"d) Suarez ",gabarito:true}
+    ],
+},
+
+];
+
+    
+   
